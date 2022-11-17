cd461b2f8c9e351eefb2208d37919f46bb6a758a
AnuradhaTomar <116050150+AnuradhaTomar@users.noreply.github.com>
2022-11-17 10:33:05 +0530

    Update Signin.js

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -8,22 +8,22 @@ export const Signin = () => {
     }
     return (
         <div>
+            <form onSubmit={Submit} className="col-lg-6 offset-lg-3">
             <h2>Add your profile</h2>
-            <form onSubmit={Submit}>
-                <div class="mb-3 my-3">
-                    <label htmlFor="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" value={exampleInputEmail1} onChange={(e) => setEmail1(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <div className="mb-3 my-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" value={exampleInputEmail1} onChange={(e) => setEmail1(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div class="mb-3">
-                    <label htmlFor="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" value={exampleInputPassword1} onChange={(e) => setPass1(e.target.value)}class="form-control" id="exampleInputPassword1"/>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" value={exampleInputPassword1} onChange={(e) => setPass1(e.target.value)}className="form-control" id="exampleInputPassword1"/>
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                        <label class="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" class="btn btn-sm btn-success">Submit</button>
+                <button type="submit" className="btn btn-sm btn-success">Submit</button>
             </form>
 
         </div>
